91064a2a28427f89ab0794c06b1b7b2044988052
hotyo <moeuvozhenie@tut.by>
2020-09-27 20:27:19 +0300

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error();
+  let resultArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-prev':
+        if (resultArray.length)
+          resultArray.pop();
+        break;
+      case '--double-prev':
+        if (resultArray.length)
+          resultArray.push(resultArray[resultArray.length - 1]);
+        break;
+      case '--discard-next':
+        if (i < arr.length - 1) {
+          i++;
+          if (arr[i + 1] === '--double-prev' || arr[i + 1] === '--discard-prev')
+            i++;
+        }
+        break;
+      case '--double-next':
+        if (i < arr.length - 1)
+          resultArray.push(arr[i + 1]);
+        break;
+      default:
+        resultArray.push(arr[i]);
+        break;
+    }
+  }
+  return resultArray;
 };
